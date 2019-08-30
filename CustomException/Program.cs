4f6de45f44e226b60013ad1c05211dc8b578efd7
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-30 10:53:24 -0400

    Add and implement CarIsDeadException.

--- CustomException/Program.cs
+++ CustomException/Program.cs
@@ -10,6 +10,22 @@ namespace CustomException
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Custom Exceptions *****\n");
+            Car myCar = new Car("Rusty", 90);
+
+            try
+            {
+                // Trip exception.
+                myCar.Accelerate(50);
+            }
+            catch (CarIsDeadException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ErrorTimeStamp);
+                Console.WriteLine(e.CauseOfError);
+            }
+
+            Console.ReadLine();
         }
     }
 }

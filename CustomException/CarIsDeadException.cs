a3885563cc10dc74960b5e07592541fb708eda8f
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-30 15:26:12 -0400

    Complete CarIsDeadException class.

--- CustomException/CarIsDeadException.cs
+++ CustomException/CarIsDeadException.cs
@@ -6,19 +6,26 @@ using System.Threading.Tasks;
 
 namespace CustomException
 {
+    [Serializable]
     class CarIsDeadException : ApplicationException
     {
         public DateTime ErrorTimeStamp { get; set; }
         public string CauseOfError { get; set; }
 
         public CarIsDeadException() { }
-
-        // Feed message to parent constructor.
+        public CarIsDeadException(string message) : base(message) { }
         public CarIsDeadException(string message, string cause, DateTime time)
-            :base(message)
+            : base(message)
         {
             CauseOfError = cause;
             ErrorTimeStamp = time;
         }
+        public CarIsDeadException(string message, System.Exception inner)
+            : base(message, inner) { }
+        protected CarIsDeadException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+        // Feed message to parent constructor.
     }
 }

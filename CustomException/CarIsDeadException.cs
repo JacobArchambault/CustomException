3df7839dd7d9460ba87703f177d1e9ab44764e51
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-30 15:14:42 -0400

    Delete message override in CarIsDead exception

--- CustomException/CarIsDeadException.cs
+++ CustomException/CarIsDeadException.cs
@@ -8,19 +8,17 @@ namespace CustomException
 {
     class CarIsDeadException : ApplicationException
     {
-        private string messageDetails = String.Empty;
         public DateTime ErrorTimeStamp { get; set; }
         public string CauseOfError { get; set; }
 
         public CarIsDeadException() { }
+
+        // Feed message to parent constructor.
         public CarIsDeadException(string message, string cause, DateTime time)
+            :base(message)
         {
-            messageDetails = message;
             CauseOfError = cause;
             ErrorTimeStamp = time;
         }
-
-        // Override the Exception.Message property.
-        public override string Message => $"Car Error Message: {messageDetails}";
     }
 }
